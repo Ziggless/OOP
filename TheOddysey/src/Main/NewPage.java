5c76f40ebe28c575e0047ab806c23fe5b69191a1
Kuromorsu <114059586+Kuromorsu@users.noreply.github.com>
2022-09-23 14:36:19 +0800

    Add files via upload
    This is for the login files, please edit for practical use. I will start storyboarding.

---
+++ TheOddysey/src/Main/NewPage.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.*;
+
+class NewPage extends JFrame
+{
+
+    NewPage()
+    {
+        setDefaultCloseOperation(javax.swing.
+                WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Welcome");
+        setSize(400, 200);
+    }
+}
